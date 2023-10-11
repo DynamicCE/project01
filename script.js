bcba058a236d024bd30bcc80fe9e9cfe797d49f6
Adınız <erkanmurat0632@gmail.com>
2023-10-11 22:07:12 +0300

    last commit and end of the project, not finished

--- script.js
+++ script.js
@@ -4,8 +4,37 @@ const form = document.querySelector(".fact-form");
 share.addEventListener("click", function () {
   if (form.classList.contains("hidden")) {
     form.classList.remove("hidden");
+    share.innerHTML = "Close";
   } else if (!form.classList.contains("hidden")) {
     form.classList.add("hidden");
+    share.innerHTML = "SHARE A FACT";
   }
 });
 // SHARE A FACT butonuna event listener ekledim
+
+// let votesInteresting = 23;
+// let votesMindblowing = 9;
+// let votesFalse = 4;
+// const text = "Lisbon is the capital of Portugal";
+// votesInteresting = votesInteresting + 1;
+// console.log(votesInteresting);
+
+// let totalUpvotes = votesInteresting + votesMindblowing;
+// console.log("Upvotes: ", totalUpvotes);
+
+// const isCorrect = votesFalse < totalUpvotes;
+
+// console.log(isCorrect);
+
+function calcFactAge(year) {
+  const currentYear = new Date().getFullYear();
+  const age = currentYear - year;
+  return age;
+}
+
+let votesInteresting = 23;
+let votesMindblowing = 9;
+let votesFalse = 4;
+if (votesInteresting === votesMindblowing) {
+  console.log("Interesting and mindblowing are equal");
+}
