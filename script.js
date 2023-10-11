cf75bb6ba4978362b2f90253351fd17399590ccd
Adınız <erkanmurat0632@gmail.com>
2023-10-11 21:44:35 +0300

    event listener

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 const share = document.querySelector("#share");
-const form = document.querySelector("fact-form");
+const form = document.querySelector(".fact-form");
+
 share.addEventListener("click", function () {
   if (form.classList.contains("hidden")) {
     form.classList.remove("hidden");
+  } else if (!form.classList.contains("hidden")) {
+    form.classList.add("hidden");
   }
 });
+// SHARE A FACT butonuna event listener ekledim
